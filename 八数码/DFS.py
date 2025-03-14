db908e4dd9364fe08b0c87086444720c02d6f18e
laxry <1849051801@qq.com>
2025-03-14 10:13:02 +0800

    “update”

--- 八数码/DFS.py
+++ 八数码/DFS.py
@@ -6,8 +6,8 @@ def get_next_states(state):
     row = idx // 3
     col = idx % 3
     next_states = []
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 上下左右四个方向
-    for dr, dc in directions:
+    dire = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 上下左右四个方向
+    for dr, dc in dire:
         new_row = row + dr
         new_col = col + dc
         if 0 <= new_row < 3 and 0 <= new_col < 3:
@@ -17,21 +17,21 @@ def get_next_states(state):
             next_states.append(tuple(new_lst))
     return next_states
 
-def is_solvable(initial):
-    stack = []
-    visited = set()
+def is_solvable(init):
+    st = []
+    vis = set()
     target = (1, 2, 3, 4, 5, 6, 7, 8, 0)
-    stack.append(initial)
-    visited.add(initial)
+    st.append(init)
+    vis.add(init)
 
-    while stack:
-        current = stack.pop()
+    while st:
+        current = st.pop()
         if current == target:
             return 1
         for next_state in get_next_states(current):
-            if next_state not in visited:
-                visited.add(next_state)
-                stack.append(next_state)
+            if next_state not in vis:
+                vis.add(next_state)
+                st.append(next_state)
     return 0
 
 def main():
@@ -42,8 +42,8 @@ def main():
             grid.append(0)
         else:
             grid.append(int(c))
-    initial = tuple(grid)
-    print(is_solvable(initial))
+    init = tuple(grid)
+    print(is_solvable(init))
 
 if __name__ == "__main__":
     main()
